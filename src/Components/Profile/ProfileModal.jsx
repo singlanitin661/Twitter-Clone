8cbb2bb25a88f5f28463c5df095f459e4b841683
singlanitin661 <singlanitin661@gmail.com>
2023-12-26 09:31:30 +0530

    Profile modal add

--- src/Components/Profile/ProfileModal.jsx
+++ src/Components/Profile/ProfileModal.jsx
@@ -1,43 +1,55 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import { useFormik } from 'formik';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useFormik } from "formik";
+import { Avatar, IconButton, TextField } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border : "none" ,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "background.paper",
+  border: "none",
   boxShadow: 24,
   p: 4,
-  outline : "none"
+  outline: "none",
+  borderRadius: 4,
 };
 
 export default function ProfileModal() {
   const [open, setOpen] = React.useState(false);
+  const [uploading, setUploading] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const handleSubmit = () =>{
-    console.log("Handle Submit") ;
-  }
+  const handleSubmit = () => {
+    console.log("Handle Submit");
+  };
 
   const formik = useFormik({
-    initialValues:{
-      fullName : "" ,
-      website : "" ,
-      location : "" ,
-      bio : "" ,
-      backgroundImage : "" ,
-      image : ""
+    initialValues: {
+      fullName: "",
+      website: "",
+      location: "",
+      bio: "",
+      backgroundImage: "",
+      image: "",
     },
-    onSubmit : handleSubmit
-  }) ;
+    onSubmit: handleSubmit,
+  });
+
+  const handleImageChange = (event) => {
+    setUploading(true);
+    const { name } = event.target;
+    const file = event.target.files[0];
+    formik.setFieldValue(name, file);
+    setUploading(false);
+  };
 
   return (
     <div>
@@ -48,15 +60,64 @@ export default function ProfileModal() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        {/* <Box sx={style}>
+        <Box sx={style}>
           <form onSubmit={formik.handleSubmit}>
-            <div className='flex items-center justify-between'>
-              <div className='flex'>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-3">
+                <IconButton onClick={handleClose} aria-label="Delete">
+                  <CloseIcon />
+                </IconButton>
+                <p className="">Edit Profile</p>
+              </div>
+              <Button type="Submit">Save</Button>
+            </div>
+            <div className="overflow-y-scroll overflow-x-hidden h-[80vh]">
+              <div>
+                <div className="w-full">
+                  <div className="relative">
+                    <img
+                      className="w-full h-[12rem] object-cover object-center"
+                      src="https://cdn.pixabay.com/photo/2022/10/23/12/50/trees-7541217_640.jpg"
+                      alt=""
+                    />
+                    <input
+                      type="file"
+                      className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                      onChange={handleImageChange}
+                      name="backgroundImage"
+                    />
+                  </div>
+                </div>
+              </div>
+      {/*  Delete the code written after this */}
+      <div className="pl-6 flex justify-between items-start mt-5 h-[5rem]">
+      <Avatar
+            alt="nitin_singla"
+            className="transform -translate-y-24"
+            src="https://e1.pxfuel.com/desktop-wallpaper/903/679/desktop-wallpaper-97-aesthetic-best-profile-pic-for-instagram-for-boy-instagram-dp-boys.jpg"
+            sx={{ width: "10rem", height: "10rem", border: "4px solid white" }}
+          />
+      </div>
+      
+      {/*  Delete the code written after this */}
 
+
+              <div className="space-y-3">
+                <TextField
+                  className="space-y-3"
+                  fullWidth
+                  id="fullName"
+                  name="fullName"
+                  label="Full Name"
+                  value={formik.values.fullName}
+                  onChange={formik.handleChange}
+                  error={formik.touched.name && Boolean(formik.errors.fullName)}
+                  helperText={formik.touched.name && formik.errors.fullName}
+                />
               </div>
             </div>
           </form>
-        </Box> */}
+        </Box>
       </Modal>
     </div>
   );
